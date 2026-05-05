b05de8f356452350a485aa5e77ac7bf996f20e1c
ninja <ninja@example.com>
2026-05-05 16:41:35 +0530

    Fix scroll position on route change

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 import Services from "./pages/Services.jsx";
@@ -7,6 +7,12 @@ import Contact from "./pages/Contact.jsx";
 import Career from "./pages/Career.jsx";
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
